988a80fe8ab5a0ab6c059761e3da5047caff3f5b
MD BISHAL <monerulmd5@gmail.com>
2024-04-03 15:33:08 +0600

    WhatsApp Integration Complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Main from "@/components/Shared/Main";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className="">
       <Main />
+      <div>
+        <Link
+          href={"https://wa.me/+8801994506240"}
+          className="fixed hidden md:block bottom-10 right-10"
+        >
+          <FaWhatsapp size={50} />
+        </Link>
+      </div>
     </main>
   );
 }
